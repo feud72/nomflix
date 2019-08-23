c5fabef9c83a3f35534add9c7a17cf0a572cecca
feud72 <feud72@gmail.com>
2019-08-23 16:53:51 +0900

    add router, css, styleComponent, Header

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
-import Home from 'Routes/Home';
-import TV from 'Routes/TV';
-import Search from 'Routes/Search';
+import React from "react";
+import { HashRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import Header from "Components/Header";
+import Home from "Routes/Home";
+import TV from "Routes/TV";
+import Search from "Routes/Search";
 
 export default () => (
   <Router>
-    <>
+	<>
+	<Header />
+    <Switch>
       <Route path="/" exact component={Home} />
-      <Route path="/tv" exact component={TV} />
-      <Route path="/search" exact component={Search} />
-    </>
+      <Route path="/tv" component={TV} />
+      <Route path="/search" component={Search} />
+	<Redirect from="*" to="/" />
+    </Switch>
+	</>
   </Router>
 );
